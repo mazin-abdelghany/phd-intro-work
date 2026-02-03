211abce64c4cf8a8ba8ba6e0583eea34820365c4
mazin-abdelghany <68043318+mazin-abdelghany@users.noreply.github.com>
2026-02-03 11:45:18 -0800

    Corrected feasilibility penalty and rebuilt Python package

--- pyGroupSequentialDesigns/build/lib/py_group_sequential_designs/feasibility_penalty.py
+++ pyGroupSequentialDesigns/build/lib/py_group_sequential_designs/feasibility_penalty.py
@@ -1,23 +1,13 @@
-from . import sample_size as ss
-
 # generate the penalty term
 def feasibility_penalty(
-        ratio=1,
-        variance=1,
+        mu,
         power=0.8,
         alpha=0.05,
-        delta=1,
         beta_prime=0.7,
         alpha_prime=0.1):
 
-    # calculate the sample size for one-stage design
-    mu = ss.sample_size_means(
-        ratio=ratio,
-        variance=variance,
-        power=power,
-        alpha=alpha,
-        delta=delta
-    )
+    # calculate beta from power
+    beta = 1-power
 
     # create the indicator functions
     def alpha_indicator(alpha_prime):
@@ -25,7 +15,7 @@ def feasibility_penalty(
         return 0
 
     def beta_indicator(beta_prime):
-        if beta_prime > power: return 1
+        if beta_prime > beta: return 1
         return 0
 
-    return mu * ( (((alpha_prime-alpha)/alpha)*alpha_indicator(alpha_prime)) + (((beta_prime-power)/power)*beta_indicator(beta_prime)) )
+    return mu * ( (((alpha_prime-alpha)/alpha)*alpha_indicator(alpha_prime)) + (((beta_prime-beta)/power)*beta_indicator(beta_prime)) )
