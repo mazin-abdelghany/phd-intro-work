e60f63f820a42180093a95ef54800e0a35666a15
mazin-abdelghany <68043318+mazin-abdelghany@users.noreply.github.com>
2026-02-13 15:39:25 -0800

    Added new line to Hello World print out

--- cpp_workspace/cpp_testing/hello_world.cpp
+++ cpp_workspace/cpp_testing/hello_world.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
-
-    return 0;
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
 }
