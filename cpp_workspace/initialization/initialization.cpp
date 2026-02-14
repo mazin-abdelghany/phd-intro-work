09e73c6ca5144d9f9b2b9de73be2f772b1bd25c5
mazin-abdelghany <68043318+mazin-abdelghany@users.noreply.github.com>
2026-02-14 09:59:34 -0800

    Assess consequence of unassigned variables

--- cpp_workspace/initialization/initialization.cpp
+++ cpp_workspace/initialization/initialization.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+void doNothing(int&) // Don't worry about what & is for now, we're just using it to trick the compiler into thinking variable x is used
+{
+}
+
 int main () {
     [[maybe_unused]] int a;
     double b = 4.5;
@@ -8,6 +12,14 @@ int main () {
     // std::cout << a << std::endl;
     std::cout << b << "\n";
     std::cout << c << std::endl;
+
+    // define an integer variable named x
+    int x; // this variable is uninitialized because we haven't given it a value
+
+    doNothing(x); // make the compiler think we're assigning a value to this variable
+
+    // print the value of x to the screen
+    std::cout << x << '\n'; // who knows what we'll get, because x is uninitialized
 
     return 0;
 }
