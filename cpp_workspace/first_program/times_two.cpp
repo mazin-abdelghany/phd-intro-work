269f29b4b85a8619f5cb793d7dc38797180391bb
mazin-abdelghany <68043318+mazin-abdelghany@users.noreply.github.com>
2026-02-15 14:10:50 -0800

    Completed quiz question

--- cpp_workspace/first_program/times_two.cpp
+++ cpp_workspace/first_program/times_two.cpp
@@ -15,10 +15,17 @@ int main()
     std::cout << "Enter an integer: ";
     int num { };
     std::cin >> num;
+    
+    std::cout << "Enter another integer: ";
+    int num_2 { };
+    std::cin >> num_2;
+
 
     std::cout << "Double " << num << " is: " << times_two(num) << "\n";
     
     std::cout << "Triple " << num << " is: " << times_three(num) << "\n";
     
+    std::cout << num << " + " << num_2 << " is " << num + num_2 << ".\n";
+    std::cout << num << " - " << num_2 << " is " << num - num_2 << ".\n";
     return 0;
 }
