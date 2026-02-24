c0f9da1be2eac325a8a5ad9ae5e4141513f106ce
mazin-abdelghany <68043318+mazin-abdelghany@users.noreply.github.com>
2026-02-24 02:03:40 -0800

    Automated data generation and consolidation into bash script

--- cpp_workspace/wason_delta_minimax/minimax.sh
+++ cpp_workspace/wason_delta_minimax/minimax.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # how many times should delta minimax run
-num_runs=100
+num_runs=2
 
 # delta minimax parameter values
 null_hypothesis=0
@@ -17,3 +17,20 @@ do
     ./finddeltaminimaxdesign $null_hypothesis $alt_hypothesis $variance \
                              $alpha $power $variance run_$i.txt
 done
+
+if [ -f new_data.txt ]; then
+    echo "Rename new_data.txt to avoid overwrites!"
+else
+    # above for loop outputs num_runs files; cat them all together into a single 
+    # file with each line showing data from iteration of loop
+    cat run*.txt > new_data.txt
+
+    # add a row at the top, in place, that names each column created by the 
+    # loops this cannot be placed on multiple lines without adding extra space  
+    # in output files
+    sed -i "1i requiredtypeIerror requiredpower K seed typeIerror power expected_sample_size_null expected_sample_size_crd expected_sample_size_dm per_group lower1 upper1 lower2 upper2 lower3 upper3" new_data.txt
+
+    # delete the num_run created files
+    rm -rf run*.txt
+fi
+
