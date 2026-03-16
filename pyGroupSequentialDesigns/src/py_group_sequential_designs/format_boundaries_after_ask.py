54f40c7de4323b9a61db0e30ebbfdde633110fea
mazin-abdelghany <68043318+mazin-abdelghany@users.noreply.github.com>
2026-03-16 13:58:55 -0700

    Added function that checks monotonicity of bounds

--- pyGroupSequentialDesigns/src/py_group_sequential_designs/format_boundaries_after_ask.py
+++ pyGroupSequentialDesigns/src/py_group_sequential_designs/format_boundaries_after_ask.py
@@ -17,3 +17,46 @@ def format_boundaries_after_ask(n_analyses, result):
         lower_bounds,
         n_patients
     ]
+
+# create a function that checks the monotonicity of bounds
+def check_monotonicity(n_analyses, bounds):
+    
+    # first format the boundaries from the ask_tell interface
+    # this takes bounds such as [upper1, upper2, upper3, lower1, lower2, n]
+    # and outputs a list of lists 
+    # [
+    #   [upper1, upper2, upper3],
+    #   [lower1, lower2, lower3], 
+    #   n
+    # ]
+    fmt_bounds = format_boundaries_after_ask(
+        n_analyses=n_analyses, 
+        result=np.array([bounds])
+    )
+
+    # take the first two indices from the list, these are the upper and lower 
+    # bounds
+    upper = fmt_bounds[0]
+    lower = fmt_bounds[1]
+
+    # loop through the bounds
+    for _i in range(len(upper)-1):
+
+        # if we are not at the last stage
+        if (_i != len(upper)-1):
+            # a design is invalid if the upper bounds are not monotonicly
+            # decreasing
+            if upper[_i] < upper[_i+1]: return False
+            # a design is invalid if the lower bounds are not monotonicly 
+            # increasing
+            if lower[_i] > lower[_i+1]: return False
+            # a design is invalid if the upper bound is not greater than the 
+            # lower bound
+            if upper[_i] <= lower[_i]: return False
+
+        # at the last stage, design is invalid if the upper bound is to the 
+        # lower bound as this is a one-sided statistical test
+        else:
+            if upper[_i] != lower[_i]: return False
+
+    return True
