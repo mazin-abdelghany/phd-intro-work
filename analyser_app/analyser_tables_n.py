d1390f609a303a3223acf473b60e0d9a8042b463
mazin-abdelghany <68043318+mazin-abdelghany@users.noreply.github.com>
2026-08-03 09:43:55 -0700

    Corrected redundant logic in index parser

--- analyser_app/analyser_tables_n.py
+++ analyser_app/analyser_tables_n.py
@@ -257,13 +257,13 @@ def parse_index(index):
     if len(s) >= 5:
         exp = int(s[:2])
         run = int(s[2:])
-        if 10 <= exp <= 99 and 100 <= run <= 99999:
+        if 10 <= exp <= 50:
             return exp, run
 
     # Otherwise it must be a 1-digit experiment (1-9)
     exp = int(s[:1])
     run = int(s[1:])
-    if 1 <= exp <= 9 and 100 <= run <= 99999:
+    if 1 <= exp <= 9:
         return exp, run
 
     raise ValueError("Invalid index")
